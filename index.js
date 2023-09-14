b0c2f68b2232f0812a2fa9717424c8aa17b92a3d
shayansheikhs <139844103+shayansheikhs@users.noreply.github.com>
2023-09-14 09:31:40 +0500

    Add files via upload
    firstcommit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+for( var a =1; a<=10;a++   ){
+    document.write(a +"<br>")
+}
+/*
+document.write(" Multiplication table of 2 length 15"+"<br>")        
+var user=+prompt("Enter length multiplication table")
+    for (var i=1; i<=15; i++  ){
+        document.write( user+ "  x  " + i +" = "+ user*i )
+document.write("<br>")        
+ }*/
+ /*
+var  arrayfruit =["apple","banana", "mango" ,"orange"]
+arrayfruit.length[0];
+// var  araayfruit= array[0]
+document.write(arrayfruit)
+if( length == 0 ){
+    document.write( "Sorry")
+
+} else{
+    document.write( "Element at index 0 is apple")
+    
+} */
+
+for( var k=0; k<=15; k++ ){
+    document.write(k)
+}
+for( var j=10; j>=1; j++ ){
+    document.write(j)
+} 
+
+    
